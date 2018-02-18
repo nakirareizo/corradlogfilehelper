c2390188356e4f6833215722aea8a5fa20b30946
najib radzuan <najibradzuan@yahoo.com>
2018-02-18 22:16:49 +0800

    completed read corrad log file from Corrad PRD server

--- CorradLogFileHelper/Form1.Designer.cs
+++ CorradLogFileHelper/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnGet = new System.Windows.Forms.Button();
+            this.dgLogFile = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgLogFile)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGet
             // 
-            this.btnGet.Location = new System.Drawing.Point(406, 44);
+            this.btnGet.Location = new System.Drawing.Point(12, 27);
             this.btnGet.Name = "btnGet";
             this.btnGet.Size = new System.Drawing.Size(99, 23);
             this.btnGet.TabIndex = 0;
@@ -42,15 +44,28 @@
             this.btnGet.UseVisualStyleBackColor = true;
             this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
             // 
+            // dgLogFile
+            // 
+            this.dgLogFile.AllowUserToAddRows = false;
+            this.dgLogFile.AllowUserToDeleteRows = false;
+            this.dgLogFile.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgLogFile.Location = new System.Drawing.Point(12, 56);
+            this.dgLogFile.Name = "dgLogFile";
+            this.dgLogFile.RowTemplate.Height = 24;
+            this.dgLogFile.Size = new System.Drawing.Size(1059, 520);
+            this.dgLogFile.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(568, 345);
+            this.ClientSize = new System.Drawing.Size(1085, 588);
+            this.Controls.Add(this.dgLogFile);
             this.Controls.Add(this.btnGet);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Corrad Log File Helper";
+            ((System.ComponentModel.ISupportInitialize)(this.dgLogFile)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,6 +73,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.DataGridView dgLogFile;
     }
 }
 
--- CorradLogFileHelper/Form1.cs
+++ CorradLogFileHelper/Form1.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using CorradLogFileHelper.Classes;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +16,12 @@ namespace CorradLogFileHelper
 {
     public partial class Form1 : Form
     {
+        NetworkCredential oNetworkCredential = new NetworkCredential()
+        {
+            Domain = "192.168.12.120",
+            UserName = "MDECDOMAIN" + "\\" + "appadmin",
+            Password = "Mdec@cyber1215"
+        };
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +29,137 @@ namespace CorradLogFileHelper
 
         private void btnGet_Click(object sender, EventArgs e)
         {
+            try
+            {
+                //DownloadFile("192.168.12.120", "appadmin", "Mdec@cyber1215", @"D:\iislogs\", "", @"C:\Users\nakirareizo");
+                using (new RemoteAccessHelper.NetworkConnection(@"\\" + "192.168.12.120", oNetworkCredential))
+                {
+                    string CorradLogFolder = @"\\192.168.12.120\D$\iislogs\";
+                    string temppath = @"C:\Users\nakirareizo\CorradLogFile";
+                    bool overwrite = false;
+                    String[] sFolderNames = Directory.GetDirectories(CorradLogFolder);
+                    foreach (String sFolderName in sFolderNames)
+                    {
+                        string LatestFile = getLatestLogFile(sFolderName);
+                        FileInfo file = new FileInfo(sFolderName + LatestFile);
+                        string LogPath = "";
+                        recursivelyCopyContents(sFolderName + "\\" + LatestFile, overwrite, temppath, out LogPath);
+                        if (LogPath != "")
+                            ReadLogFile(LogPath);
+                        //string[] sarrZipFiles = Directory.GetFiles(sFolderName, "*.log");
+                        //foreach (String sFile in sarrZipFiles)
+                        //{
+                        //    FileInfo TheFile = new FileInfo(sFile);
+                        //}
+                        //if (CopyFiles(false, "192.168.12.120", sFolderName, @"C:\Users\nakirareizo\CorradLogFile"))
+                        //    ReadLogFile(sFolderName, LatestFile);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+        }
 
+        private void ReadLogFile(string LogFile)
+        {
+            ArrayList LogLine = new ArrayList();
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(LogFile))
+            {
+                String line;
+                while ((line = sr.ReadLine()) != null)
+                {
+
+                    LogLine.Add(line);
+
+                }
+            }
+            DataTable dtLog = new DataTable();
+            dtLog.Columns.Add("IP");
+            dtLog.Columns.Add("DateCalled");
+            dtLog.Columns.Add("TimeCalled");
+            dtLog.Columns.Add("APIParam");
+            foreach (string item in LogLine)
+            {
+                //0: Requestor IP
+                //2: Date Called
+                //3: Time Called
+                //14: API Name &Parameter
+                string[] strLine = item.Split(',');
+                DataRow dr = dtLog.NewRow();
+
+                dr[0] = strLine[0].ToString();
+                dr[1] = strLine[2].ToString();
+                dr[2] = strLine[3].ToString();
+                dr[3] = strLine[14].ToString();
+                dtLog.Rows.Add(dr);
+
+            }
+            BindToGridView(dtLog);
+        }
+
+        private void BindToGridView(DataTable dtLog)
+        {
+            dgLogFile.DataSource = dtLog;
+        }
+
+        private string getLatestLogFile(string directory)
+        {
+            string latestfile = "";
+            DirectoryInfo Direc = new DirectoryInfo(directory);
+
+            FileInfo[] FoldersArr = Direc.GetFiles();
+
+            if (FoldersArr.Length > 0)
+            {
+                DateTime MostRecent = FoldersArr[1].LastWriteTime;
+                FileInfo RecentFolder;
+
+                foreach (FileInfo f in FoldersArr)
+                {
+                    if (MostRecent.CompareTo(f.LastWriteTime) < 0)
+                    {
+                        MostRecent = f.LastWriteTime;
+                        RecentFolder = f;
+                        latestfile = RecentFolder.ToString();
+                    }
+                }
+
+
+            }
+            return latestfile;
+
+        }
+
+        private void recursivelyCopyContents(string sourceLogFile, bool overwrite, string destDirName, out string LogPath)
+        {
+            LogPath = "";
+            try
+            {
+                FileInfo file = new FileInfo(sourceLogFile);
+                string temppath = Path.Combine(destDirName, file.Name);
+
+                string info = "Copying " + file.Name + " -> " + temppath;
+
+
+                if (!overwrite && File.Exists(temppath))
+                {
+                    // Not overwriting - so show it already exists
+                    LogPath = temppath;
+                    MessageBox.Show(info + " (already exists)");
+                }
+                else
+                {
+                    file.CopyTo(temppath, overwrite);
+                    LogPath = temppath;
+                    //MessageBox.Show(info + " OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problem encountered during copy : " + ex.Message);
+            }
         }
     }
 }
